24a409030448f53285bb66247685a756b2aa0253
Ethan Frazier <zenedf@protonmail.com>
2022-03-21 13:25:06 -0400

    Fixed windows output.

--- portscanner.py
+++ portscanner.py
@@ -9,17 +9,6 @@ from termcolor import colored
 from socket    import socket
 from os        import system, name
 
-def clear_screen():
-    system("clear")
-
-def introduction():
-    clear_screen()
-    print(colored(("Port Scanner v.1 by Ethan Frazier \n"), 'red'))
-
-def scan(target, ports):
-    print('\n' + ' Starting Scan For ' + str(target))
-    for port in range(1,ports):
-        scan_port(target,port)
 
 def scan_port(ipaddress, port):
     try:
@@ -29,6 +18,21 @@ def scan_port(ipaddress, port):
         sock.close()
     except: 
         pass # it didn't connect {closed, filtered, or ignored ports}
+
+def scan(target, ports):
+    print('\n' + ' Starting Scan For ' + str(target))
+    for port in range(1,ports):
+        scan_port(target,port)
+
+def clear_screen():
+    if name != "posix":
+         system("cls")
+    else:
+        system("clear")
+
+def introduction():
+    clear_screen()
+    print(colored(("Port Scanner v.1 by Ethan Frazier \n"), 'red'))
 
 def main():
     ''' Main logic of the program '''
